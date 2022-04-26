5e4c6db352a3630ed825dcff035db7ae7dcee0d4
Azlan Sabir <68316056+AzlanCrypt@users.noreply.github.com>
2022-04-26 12:30:28 +0530

    Add files via upload

---
+++ src/FirebaseConfig/firebaseConfig.js
@@ -0,0 +1,10 @@
+export default {
+  apiKey: "AIzaSyC--YRZ3ohuUDUvIpsmDO_dbFynHsXvB2A",
+  authDomain: "mygitapp-33865.firebaseapp.com",
+  databaseURL: "https://mygitapp-33865.firebaseio.com",
+  projectId: "mygitapp-33865",
+  storageBucket: "mygitapp-33865.appspot.com",
+  messagingSenderId: "487517126222",
+  appId: "1:487517126222:web:926aec31e36e3089430d1f",
+  measurementId: "G-RCEXWW86PY",
+};
